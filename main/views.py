2a5f39cd2f42b4a96a379a07f34c44a3f2911ed4
Aisuluu1980 <aisuluueco2009@yandex.ru>
2021-01-11 10:24:40 +0600

    init commit

---
+++ main/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+
+
+def homepage(request):
+    return HttpResponse('Hello Aisl!!!')
+
+
+def test(request):
+    return render(request, 'test.html')
+
+
+def go(request):
+    return render(request, 'go.html')
