74e01a35839538ed20b002606673a8257f73bfb0
Aisuluu1980 <aisuluueco2009@yandex.ru>
2021-01-20 16:22:25 +0600

    Added todo closing

--- todo/urls.py
+++ todo/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from main.views import homepage, third, add, update, delete, mark_todo, unmark_todo
+from main.views import homepage, third, add, update, delete, mark_todo, unmark_todo, close_todo
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,5 +29,6 @@ urlpatterns = [
     path('delete/<id>', delete, name='delete'),
     path('mark/<id>', mark_todo, name='mark_todo'),
     path('unmark/<id>', unmark_todo, name='unmark_todo'),
+    path('closed/<id>', close_todo, name='close_todo'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
